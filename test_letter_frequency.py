55355a7d7aa33851f1eee643e402a77b1fbff7ac
RafaelPinto <rafaelpinto.ve@gmail.com>
2024-06-05 21:44:27 -0500

    TST: Should encrypt message with rot13

--- test_letter_frequency.py
+++ test_letter_frequency.py
@@ -3,7 +3,7 @@ import tempfile
 import unittest
 from pathlib import Path
 
-from letters_frequency import letters_count, letters_in_file
+from letters_frequency import caesar_cipher, letters_count, letters_in_file
 
 TEST_TEXT = """
 Mr. Jock, TV quiz PhD, bags few lynx.
@@ -33,4 +33,33 @@ class TestFileParser(unittest.TestCase):
         self.assertEqual(
             count,
             {letter: 2 for letter in string.ascii_lowercase}
+        )
+
+
+class TestCaesarCipher(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.tmpdir = tempfile.TemporaryDirectory()
+        cls.addClassCleanup(cls.tmpdir.cleanup)
+        cls.test_text_path = Path(cls.tmpdir.name) / "test_text.txt"
+
+        with open(cls.test_text_path, "w+") as fhandle:
+            fhandle.write(TEST_TEXT)
+
+    def test_rot13(self):
+        encrypted_text_path = Path(self.tmpdir.name) / "encrypted_text.txt"
+
+        caesar_cipher(
+            clear_text_path=self.test_text_path,
+            rot=13,
+            output_path=encrypted_text_path,
+        )
+
+        with open(encrypted_text_path, "r") as fhandle:
+            encrypted_text = "\n".join(fhandle.readlines())
+
+        self.assertEqual(
+            encrypted_text,
+            "Ze. Wbpx, GI dhvm CuQ, ontf srj ylak.\n"
+            "Ze. Wbpx, GI dhvm CuQ, ontf srj ylak."
         )
