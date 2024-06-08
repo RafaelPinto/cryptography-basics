0b77f1b4a2228ae0032dfe3719691ef0e684e1eb
RafaelPinto <rafaelpinto.ve@gmail.com>
2024-06-08 07:05:06 -0500

    ENH: Add caesar cipher

--- letters_frequency.py
+++ letters_frequency.py
@@ -1,6 +1,7 @@
 import re
 from collections import Counter
 from pathlib import Path
+import string
 
 
 def letters_in_file(filepath: Path):
@@ -11,3 +12,26 @@ def letters_in_file(filepath: Path):
 
 def letters_count(filepath: Path):
     return Counter(letters_in_file(filepath))
+
+
+def caesar_cipher(clear_text_path: Path, rot: int, output_path: Path):
+    """
+    Substitute the letters in the `clear_path_text` by those after
+    rotating the alphabet `rot` times. Write the output to the given
+    `output_path`.
+    """
+    alphabet = string.ascii_lowercase
+    rot = rot % len(alphabet)
+    alphabet_rot = alphabet[rot:] + alphabet[:rot]
+    trans_table = str.maketrans(
+        alphabet + alphabet.upper(),
+        alphabet_rot + alphabet_rot.upper(),
+    )
+
+    with (
+        open(clear_text_path, 'r') as reader,
+        open(output_path, 'w') as writer
+    ):
+        for line in reader:
+            writer.write(line.translate(trans_table))
+
