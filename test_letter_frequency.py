eb11e34381cee69fe611f0ec5dd116739b8fa54d
RafaelPinto <rafaelpinto.ve@gmail.com>
2024-06-19 05:25:33 -0500

    TST: Should capture the first page

--- test_letter_frequency.py
+++ test_letter_frequency.py
@@ -104,6 +104,32 @@ class TestPDFParser(unittest.TestCase):
             expected_letter_count,
         )
 
+    def test_count_pdf_file_letters_first_single_page(self):
+        expected_text = """
+            This is the header to be ignored
+            This is the first paragraph on the first page.
+            This is the second paragraph on the first page.
+            This is the footer to be ignored
+        """
+        expected_letter_count = Counter(
+            [
+                char.lower()
+                for char in expected_text
+                if char.lower() in string.ascii_lowercase
+            ]
+        )
+
+        self.assertEqual(
+            count_pdf_file_letters(
+                self.test_pdf_path,
+                page_start=0,
+                page_stop=1,
+                y_min=50,
+                y_max=760,
+            ),
+            expected_letter_count,
+        )
+
 
 class TestCaesarCipher(unittest.TestCase):
     @classmethod
